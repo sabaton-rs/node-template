fba91b3960e1029d61962a95f2e5711780d001c4
Sojan James <sojan.james@gmail.com>
2022-03-25 10:46:13 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<div align="center">
+
+# Template for a node that you can use with cargo-generate
+
+## Documentation
+
+See the `cargo-generate` [guide](https://cargo-generate.github.io/cargo-generate/index.html) for complete documentation.
+
+## Quickstart
+### Installation
+
+```sh
+cargo install cargo-generate
+```
+
+### Usage
+
+```sh
+# templates on github
+cargo generate --git https://github.com/sabaton-rs/node-template.git
+
+```
---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+#![forbid(unsafe_code)]
+
+use std::time::Duration;
+// Main Library file
+use sabaton_mw::{NodeBuilder, error::MiddlewareError};
+use tracing::{debug, info, span, Level};
+
+pub fn example_node_main() -> Result<(),MiddlewareError> {
+
+    let node =   NodeBuilder::default()
+        //.multi_threaded()  Enable this if you want a multi-threaded runtime
+        .with_namespace("namespace".into())
+        //.with_num_workers(4)    // Number of work threads. Fixed to 1 for single threaded runtime.
+        .build("example-node".to_owned()).expect("Node creation error");
+
+    let res = node.spin(|| {
+        
+        span!(target: "MAIN", Level::TRACE, "Application Main Loop");
+        info!("Application Main Loop Started with tick interval 100mS");
+
+        let mut ticker = tokio::time::interval(Duration::from_millis(100));
+
+        let _task = tokio::spawn( async move {
+
+            loop {
+                let _ = ticker.tick().await;
+                debug!("Tick");
+            }
+
+         });
+         
+    });
+
+
+    res
+
+}
